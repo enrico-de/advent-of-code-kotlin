59f1c447a88bd35d25aecb9abc4cbb33f6a1f29c
Enrico Kienel <enrico.k@freenet.de>
2021-12-06 20:26:24 +0100

    Day03 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,32 +1,50 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val bitCount = input.maxOf { it.length }
-        var gammaRate = 0
+    /**
+     * @param idx index of the bit in question (starting from the LSB)
+     * @return 1 if the bit set, else 0
+     */
+    operator fun Int.get(idx: Int) = this shr idx and 1
 
-        for (i in 0 until bitCount) {
-            if (input.map { it.toInt(2) }.map { it shr i }.count { it.mod(2) == 1 } > input.size / 2)
-                gammaRate = gammaRate or (1 shl i)
-        }
+    fun List<String>.asNumbers() = map { it.toInt(2) }
+
+    /**
+     * @param idx index of the bit in question (starting from the LSB)
+     * @param numbers taken into account to determine the most common bit
+     * @return 1 if the idx bit is set in more numbers than it is unset, else 0
+     */
+    fun mostCommon(idx: Int, numbers: List<Int>) = numbers.count { it[idx] == 1 } * 2 / numbers.size
+
+    fun powerConsumption(numbers: List<Int>, bitCount: Int): Int {
+        val gammaRate = IntRange(0, bitCount - 1)
+                .map { mostCommon(it, numbers) shl it }
+                .fold(0) { acc, i -> acc or i }
 
         val epsilonRate = (1 shl bitCount) - gammaRate - 1
         return gammaRate * epsilonRate
     }
 
-    fun part2(input: List<String>): Int {
-        val bitCount = input.maxOf { it.length }
+    fun lifeSupportRating(numbers: List<Int>, bitCount: Int): Int {
+        val numbers1 = numbers.toMutableList()
+        val numbers2 = numbers.toMutableList()
 
-//        for (i in bitCount - 1 downTo 0) {
-//        }
+        for (i in bitCount - 1 downTo 0) {
+            val mostCommon = mostCommon(i, numbers1)
+            if (numbers1.size > 1)
+                numbers1.retainAll { it[i] == mostCommon }
 
-        val oxygenGeneratorRating = 0
-        val co2ScrubberRating = 0
+            val leastCommon = mostCommon(i, numbers2) xor 1
+            if (numbers2.size > 1)
+                numbers2.retainAll { it[i] == leastCommon }
+        }
+
+        val oxygenGeneratorRating = numbers1.single()
+        val co2ScrubberRating = numbers2.single()
         return oxygenGeneratorRating * co2ScrubberRating
     }
 
-    fun mostCommon(index: Int, input: List<Int>) = if (input
-        .map { it shr index }
-        .count { it.mod(2) == 1 } > input.size / 2) 1 else 0
+    fun part1(input: List<String>) = powerConsumption(input.asNumbers(), input.maxOf { it.length })
+    fun part2(input: List<String>) = lifeSupportRating(input.asNumbers(), input.maxOf { it.length })
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
